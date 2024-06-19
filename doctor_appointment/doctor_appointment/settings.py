1a9f5a96385cdcdae87e5f546f9343f1bb1c84fa
saimwafa <146020175+saimwafa@users.noreply.github.com>
2024-06-19 23:05:33 +0500

    new push

--- doctor_appointment/doctor_appointment/settings.py
+++ doctor_appointment/doctor_appointment/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'appointments',
+    'corsheaders'
 ]
 
 REST_FRAMEWORK = {
@@ -60,7 +61,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'doctor_appointment.urls'
 
